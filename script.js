df3cf47ce51f357f1aee906f0796f81bd613fd6d
Ismaila Afeez <157530759+Harflexdev@users.noreply.github.com>
2024-06-17 11:18:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function davii() {
+    const navMenu = document.querySelector('.nav-menu');
+    const menu = document.querySelector('.menu');
+    navMenu.classList.toggle('menu-open');
+    menu.classList.toggle('popUp');
+}
